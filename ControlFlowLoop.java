53078c3ffd3e165850b20f657c7757d8273bc9da
VinitaBapna <51161417+VinitaBapna@users.noreply.github.com>
2023-10-30 00:21:36 +0530

    Create ControlFlowLoop.java

---
+++ ControlFlowLoop.java
@@ -0,0 +1,16 @@
+package arithmetic;
+
+import java.util.Scanner;
+
+public class ControlFlowLoop
+{
+	public static void main(String[] args)
+	{
+		int i = 1;
+		while(i<=10)
+		{
+			System.out.println(i);
+			i++;
+		}
+	}
+}
